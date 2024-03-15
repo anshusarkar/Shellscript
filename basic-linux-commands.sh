25860651e665e326ead264ed8cdce12ae609be70
anshusarkar <anshusarkar11@gmail.com>
2024-03-15 19:29:08 +0530

    echo

---
+++ basic-linux-commands.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+pwd # To check present working directory
+
+ls # To list the files in a directory i6
+
+cd # To change the current directory to home directory
+
+cd .. # To go back one directoy
+
+~/cd/"Direcory-name" # To navigate to specific directory
+
+sudo su # To make the users super user i.e. giving the users the privilage for root 
+
+date # To check the date 
+
+echo # To print something in the 
+
+cat filename # Is the command to print the contens of a file 
+
+cat > filename # Is the command to write into the file in the termminal (use ctrl +d to save the file )
+
+chmod +x # is the command that makes a script system executable
+
+mkdir # is the command that get's used to make directory
+
+cp ../"A-file-from-parent-directory" . # To copy a file from parent directory to present child direcory the .. part states the parent directory and the . states the currrent directory 
+
+touch "filename.extension" # To make an empty file
+
+ls -l # To list files in a directoy with their read-write permission
+
+# lots of the command has a -l part they are called flags or delemetrs 
+# to print all the avalable delemeters of a command use the --help of -h with the command to list them
+
